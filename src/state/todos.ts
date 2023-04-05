fa47704e4f64b2d56ce7c05c391ba01d8f27ec1d
Alon Dubov <alon@dubov.tech>
2023-04-05 13:18:35 +0300

    Initial commit with nextjs, @mui, and recoiljs

---
+++ src/state/todos.ts
@@ -0,0 +1,22 @@
+import { atom } from "recoil";
+
+export interface TodoItem {
+    id?: string;
+    last_synced?: Date;
+    text: string;
+    synced_text?: string;
+    completed: Boolean;
+}
+
+export const todoItemsState = atom({
+    key: "todos/items",
+    default: []
+});
+
+export const activeTodoState = atom<TodoItem>({
+    key: "todos/activeItem",
+    default: {
+        text: "",
+        completed: false
+    }
+});
