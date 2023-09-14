4baaa43d5aa44f6ea5bf8328e104bc7be1ac6bbd
Alon Dubov <alon@alon.codes>
2023-09-14 22:09:55 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    // Django backend
+    NEXT_SERVICE_URL: "https://todo-items-django-server.vercel.app"
+  }
 }
 
 module.exports = nextConfig
