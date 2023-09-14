b010a31416aef92ea48ff66feb8f7f4b947fb31a
Alon Dubov <alon@alon.codes>
2023-09-14 22:10:33 +0300

    Update proxy.ts

--- src/server/proxy.ts
+++ src/server/proxy.ts
@@ -5,6 +5,6 @@ export const proxy = httpProxy.createProxyServer({
    * Get the actual bac-end service url from env variables.
    * We shouldn't prefix the env variable with NEXT_PUBLIC_* to avoid exposing it to the client.
    */
-  target: process.env.SERVICE_URL,
+  target: process.env.NEXT_SERVICE_URL,
   autoRewrite: false
 });
